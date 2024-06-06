afc59a640fb44bfd6c0ae1c833bf8c7a54f5f74c
rngnnn <rngnnnclk@gmail.com>
2024-06-06 10:42:42 +0200

    DRAW

--- app.js
+++ app.js
@@ -3,6 +3,11 @@
 let yourCoiceDiv = document.getElementById("your-choice")
 const pcCoiceDiv = document.getElementById("pc-choice")
 
+//& message
+
+const messagePar = document.querySelector(".message")
+
+
 //& Colors
 const YELLOW = "#ffc538";
 const RED = "#fb778b";
@@ -10,10 +15,14 @@ const GREEN = "#5ab7ac";
 
 //& Variables
 const userSelectImg = document.createElement("img")
-const pcSelectImg=document.createElement("img")
+const pcSelectImg = document.createElement("img")
 let userSelection; //kullanıcının seçtiği
 let pcRandom; // pc nin rastgele ürettiği
 let pcArr = []; //pc seçim dizisi
+
+//& Score
+const scoreCardSection = document.querySelector(".score-card")
+
 
 //! Selection
 const selectionArticle = document.querySelector(".selection")
@@ -28,6 +37,7 @@ selectionArticle.addEventListener("click",(e)=>{
     }
 
     createPCSelection()
+    
 
 })
 
@@ -35,13 +45,25 @@ const createPCSelection = () => {
     pcArr = ["rock", "paper", "scissor" ]
     // pcRandom =Math.floor(Math.random()*3) //1. yöntem
     pcRandom = pcArr[Math.trunc(Math.random()*3)]  //2. yöntem
-  // console.log(pcRandom)
-  pcSelectImg.src = `./assets/${pcRandom}.png`
-  pcCoiceDiv.appendChild(pcSelectImg)
+    // console.log(pcRandom)
+    pcSelectImg.src = `./assets/${pcRandom}.png`
+    pcCoiceDiv.appendChild(pcSelectImg)
+
+    calculateResult()
+
 }
 
-const calculateResult=()=>{
-console.log(userSelection,pcRandom);
+const calculateResult = () => {
+    // console.log(userSelection , pcRandom)
 
+    if (userSelection == pcRandom) {
+        draw()
+    }
+}
+
+const draw = () => {
+    messagePar.textContent = "it's a draw";
+    scoreCardSection.style.color = YELLOW;
+    messagePar.style.backgroundColor = YELLOW
 
 }
